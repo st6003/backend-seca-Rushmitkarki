d4491a4daa6cc5080f828cb10320dbbd335b1234
Rushmitkarki <220046@softwarica.edu.np>
2024-06-11 16:40:03 +0545

    role managment

--- models/userModel.js
+++ models/userModel.js
@@ -1,26 +1,34 @@
 const mongoose = require("mongoose");
+
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    requires: true,
+    required: true,
   },
   lastName: {
     type: String,
-    requires: true,
+    required: true,
   },
   email: {
     type: String,
-    requires: true,
+    required: true,
   },
   password: {
     type: String,
-    requires: true,
+    required: true,
   },
   conformPassword: {
     type: String,
-    requires: true,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    required: true,
+    default: "user",
   },
 });
+
 const User = mongoose.model("users", userSchema);
 
 module.exports = User;
