05fde60be49d39afc6743ebed43ea4b1af49772c
Rushmitkarki <220046@softwarica.edu.np>
2024-06-24 22:33:05 +0545

    add doctor pagination

--- controllers/doctorControllers.js
+++ controllers/doctorControllers.js
@@ -168,7 +168,7 @@ const paginationDoctors = async (req, res) => {
   // page no
   const PageNo = req.query.page || 1;
   // per page count
-  const resultPerPage = 4;
+  const resultPerPage = 2;
 
   try {
     // find all doctors, skip , limit
