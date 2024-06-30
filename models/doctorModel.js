914ae0451024b625dfb18cd94750f5ff89d7ef9a
Rushmitkarki <220046@softwarica.edu.np>
2024-06-30 10:55:57 +0545

    added doctors for testing

--- models/doctorModel.js
+++ models/doctorModel.js
@@ -1,32 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const doctorSchema = new mongoose.Schema({
-    doctorName: {
-        type: String,
-        required: true,
-    },
-    doctorField: {
-        type: String,
-        required: true,
-    },
-    doctorExperience: {
-        type: Number,
-        required: true,
-    },
-    doctorFee: {
-        type: Number,
-        required: true,
-    },
-    doctorImage: {
-        type: String,
-        required: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-    },
-
-
+  doctorName: {
+    type: String,
+    required: true,
+  },
+  doctorField: {
+    type: String,
+    required: true,
+  },
+  doctorExperience: {
+    type: Number,
+    required: true,
+  },
+  doctorFee: {
+    type: Number,
+    required: true,
+  },
+  doctorImage: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
-const Doctor = mongoose.model('Doctor', doctorSchema);
+const Doctor = mongoose.model("Doctor", doctorSchema);
 module.exports = Doctor;
