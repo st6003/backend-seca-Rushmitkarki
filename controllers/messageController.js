00f5d8139e7c34b224e049a7737229f6ec0fff8f
Rushmitkarki <220046@softwarica.edu.np>
2024-07-17 18:34:22 +0545

    changes controller

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,10 +1,9 @@
+const mongoose = require("mongoose");
 const Message = require("../models/messageModel");
 const User = require("../models/userModel");
 const Chat = require("../models/chatModel");
 
-//@description     Get all Messages
-//@route           GET /api/Message/:chatId
-//@access          Protected
+// Get all Messages
 const allMessages = async (req, res) => {
   try {
     const messages = await Message.find({ chat: req.params.chatId })
@@ -17,15 +16,18 @@ const allMessages = async (req, res) => {
   }
 };
 
-//@description     Send a Message
-//@route           POST /api/message/
-//@access          Protected
+// Send a Message
 const sendMessage = async (req, res) => {
   const { content, chatId } = req.body;
 
   if (!content || !chatId) {
     console.log("Invalid data passed into request");
     return res.sendStatus(400);
+  }
+
+  // Validate chatId
+  if (!mongoose.Types.ObjectId.isValid(chatId)) {
+    return res.status(400).json({ message: "Invalid chatId" });
   }
 
   const newMessage = {
@@ -37,16 +39,16 @@ const sendMessage = async (req, res) => {
   try {
     let message = await Message.create(newMessage);
 
-    message = await message
-      .populate("sender", "firstName")
-      .populate("chat")
-      .execPopulate();
-    message = await User.populate(message, {
-      path: "chat.users",
-      select: "firstName",
-    });
+    message = await message.populate("sender", "firstName").execPopulate();
+    message = await message.populate({
+      path: "chat",
+      populate: {
+        path: "users",
+        select: "firstName",
+      },
+    }).execPopulate();
 
-    await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: message });
+    await Chat.findByIdAndUpdate(chatId, { latestMessage: message });
 
     res.json(message);
   } catch (error) {
